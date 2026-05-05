aa55f2b264dc441c3be0d44b90b3277fde041913
AlanaPriscila123 <alana.marques.ramos@escola.pr.gov.br>
2026-05-05 12:29:25 -0300

    Add files via upload

---
+++ src/componentes/Footer.js
@@ -0,0 +1,22 @@
+function Footer(){
+
+    return (
+        <footer>
+        <div className="menuFooter">
+            <ul>
+                <li>Sobre o curso</li>
+                <li>Disciplinas Curriculares</li>
+                <li>Disciplinas Técnicas</li>
+                <li>Equipe</li>
+            </ul>
+        </div>
+        <div className="credencias">
+            <h1>Desenvolvido por: Alana Priscila Marques Ramos - 2026</h1>
+        </div>
+        </footer>
+            
+    )
+}
+
+export default Footer;
+
--- src/componentes/Main.js
+++ src/componentes/Main.js
@@ -7,3 +7,4 @@ function Main(){
     )
 }
 
+export default Main;
